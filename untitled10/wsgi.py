cf74101b7b6ef728da79f979c1077ec895647573
code-zyp1 <zpenva123@gmail.com>
2020-05-21 10:13:31 +0800

    1

--- untitled10/wsgi.py
+++ untitled10/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+
+sys.path.append('/usr/local/python3/lib/python3.6/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled10.settings')
 
 application = get_wsgi_application()
